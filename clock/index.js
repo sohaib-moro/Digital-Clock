e8c2914ddc504920029b14a72889ef4fedb8f4b0
sohaib-moro <bensaidisohaib@gmail.com>
2025-10-22 19:24:07 +0100

    Add files via upload

---
+++ clock/index.js
@@ -0,0 +1,18 @@
+function updateClock(){
+
+    const now = new Date () ;
+    let hours = now.getHours();
+    const ap = hours>= 12 ? "PM" :"AM"
+    hours = hours % 12 || 12 ;
+    hours = hours.toString().padStart(2,0);
+    const minutes = now.getMinutes().toString().padStart(2,0) ;
+    const seconds = now.getSeconds ().toString().padStart(2,0);
+    const timestring = `${hours}:${minutes}:${seconds} ${ap}`
+    document.getElementById(`clock`).textContent = timestring ;
+
+
+
+}
+
+updateClock();
+setInterval(updateClock,1000);
